56c67202b5cd921825338d49e7204e1c6801be4a
Dev karan <dev.karan@ymedialabs.com>
2023-04-06 18:37:26 +0530

    [CM-1300] Comments updated.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -17,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/adobe/aepsdk-analytics-ios.git",
-            .upToNextMajor(from: "3.0.0")
+            .upToNextMajor(from: "3.2.0")
         ),
         .package(
             url: "https://github.com/yml-org/yanalytics-ios.git",
